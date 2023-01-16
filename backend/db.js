54ef8298d0faa07de3bf35f1fec4195b965144ea
root_2 <yolotanvir@gmail.com>
2023-01-17 01:29:57 +0530

    database change and readme change

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const mongoURI =
-  "mongodb://localhost:27017/iNotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+  "mongodb://squareroottwo:Q9Vc5jBvnfCRdomDF0SvNNFwFwwwcMT86v5vm73ZdnhuwWT7IFGuYSzsv8Sdiy4wg2qUvHS441HkACDbV20lQg%3D%3D@squareroottwo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@squareroottwo@";
+//"mongodb://localhost:27017/iNotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
